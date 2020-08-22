2443bfd5f2168fc7cb3ef35cfd84b50b3c852bdc
sebastien.flamant <sebastien.flamant@insidegroup.fr>
2020-08-22 17:14:29 +0200

    Update StandardGradeBook Type

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -10,7 +10,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
