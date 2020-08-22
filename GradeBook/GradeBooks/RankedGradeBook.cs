8b53c9fb678e508068aea9b0c49b052749485f40
sebastien.flamant <sebastien.flamant@insidegroup.fr>
2020-08-22 21:04:48 +0200

    Create GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,14 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else return 'F';
+        }
     }
 }
